9a496222cc2c9d0cb4c6757a248155872ee80f8f
CarFal <carlosjf60@gmail.com>
2021-03-10 12:26:31 -0500

    Register Page Logic

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -10,10 +10,13 @@ import { UserDataService } from "../user-data.service";
 })
 export class RegisterComponent implements OnInit {
 
+  errorMSG: string = '';
   pregex : RegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/; //one digit, one lower case, one uppercase\
   emailRegex: RegExp = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/; //email regex
   valUserName: boolean = false;
   valPassword: boolean = false;
+  valEmail: boolean = false;
+  valConfirmPass: boolean = false;
   registUser: User = {
     UserName: "", 
     Email: "", 
@@ -33,33 +36,74 @@ export class RegisterComponent implements OnInit {
   }
   initializeForm(): void {
     this.accountForm = this.fb.group({
-      name: 'Enter name',
-      password: 'Enter password',
-      conPassword: 'Enter Password Again',
-      email: 'enter email',
+      name: "",
+      password: "",
+      conPassword: "",
+      email: "",
+      age: false,
       term: false
     });
   }
 
   onSubmit(): void{
     const formValue = this.accountForm.value;
-    //if(this.valPassword && formValue.term && )
+    if(formValue.password != null ){
+      if(this.pregex.test(formValue.password)){
+        this.valPassword = true;
+      }
+    }
+    if(this.accountForm.get("password")?.value === this.accountForm.get("conPassword")?.value){
+      this.valConfirmPass = true;
+    }
+    if(formValue.email != null){
+      this.valEmail = true;
+    }
+    if(formValue.name != null && formValue.name != "" && formValue.name.length >= 3){
+      this.valUserName = true;
+    }
+    if(this.valPassword && formValue.term && this.valEmail && this.valConfirmPass && formValue.age && this.valUserName){
+      console.log(this.accountForm);
+      console.log(formValue)
+      this.registUser.UserName = formValue.name;
+      this.registUser.Email = formValue.email;
+      this.registUser.Password = formValue.password;
+      this.registUser.Date = new Date();
+      this.registUser._id = this.generateUniqueID();
+      
+      console.log(this.registUser);
+      this.data.newUser(this.registUser);
+    }
+    else if(!formValue.term){
+      //alert box for unchecked terms
+      this.errorMSG = "Terms not accepted."
+      //console.log("Terms not accepted.");
+    }
+    else if(!formValue.age){
+      //Alet box for underage
+      this.errorMSG = "You must be over 13 years old to create an account."
+      //console.log("You must be over 13 years old to create an account.");
+    }
+    else if(!this.valUserName){
+      //alert box for invalid username
+      this.errorMSG = "Invalid Username."
+      //console.log("Invalid Username");
+    }
+    else if(!this.valPassword){
+      //alert box for invalid Password
+      this.errorMSG = "Invalid Password."
+      //console.log("Invalid Password");
+    }
+    else if(!this.valConfirmPass){
+      //alert box for invalid confirmation passowrd
+      this.errorMSG = "Doesn't match up with Password."
+      //console.log("Doesn't match up with Password");
+    }
+    else if(!this.valEmail){
+      //alert box for invalid email address
+      this.errorMSG = "This email address already exists."
+      //console.log("This email address already exists");
+    }
 
-    
-    console.log(this.accountForm);
-    console.log(formValue)
-    this.registUser.UserName = formValue.name;
-    this.registUser.Email = formValue.email;
-    this.registUser.Password = formValue.password;
-    this.registUser.Date = new Date();
-    //this.registUser.Balance = 5000;
-    //this.registUser.ProfilePic = "defaultPic.png";
-    //this.registUser.IsAdmin = false;
-    //this.registUser._v = 0;
-    this.registUser._id = this.generateUniqueID();
-    
-    console.log(this.registUser);
-    this.data.newUser(this.registUser);
   }
 
   generateUniqueID(): number{
@@ -68,6 +112,5 @@ export class RegisterComponent implements OnInit {
     let num = Math.random() * Math.floor(101);
     return letter + ranNumb + num;
   }
-
 
 }
