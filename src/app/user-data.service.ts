abcacab579d496fb7de2bf18d2d767a19599d855
zg3d <devzs6.98@gmail.com>
2021-03-11 17:43:48 -0500

    changed user-data.service

--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { User } from './user.model';
 import { Observable } from 'rxjs';
-
+const API_URI='https://allinsserservice.herokuapp.com';
 @Injectable({
   providedIn: 'root'
 })
@@ -11,10 +11,21 @@ export class UserDataService {
   constructor(private http: HttpClient) { }
 
   getUsers():Observable<User[]> {
-    return this.http.get<User[]>(`https://allinsserservice.herokuapp.com/v1/users?page=1&perPage=10`);
+    const perPage = '' + Number.MAX_SAFE_INTEGER;
+    return this.http.get<User[]>(`${API_URI}/v1/users?page=1&perPage=${perPage}`);
   }
 
-  newUser(newUser: User):Observable<any>{
-    return this.http.post<any>(`https://allinsserservice.herokuapp.com/v1/user`, newUser);
+  getUserById(userId : string):Observable<User>{
+    return this.http.get<User>(`${API_URI}/v1/users/${userId}`);
   }
+
+  register(newUser: User):Observable<any>{
+    return this.http.post<any>(`${API_URI}/v1/user`, newUser);
+  }
+
+  update(user:User):Observable<any>{
+    return this.http.put<any>(`${API_URI}/v1/users/${user._id}`,user);
+  }
+
+
 }
