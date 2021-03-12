4aece945684a1b80d65137b2c85f1fde9e18d5c8
CarFal <carlosjf60@gmail.com>
2021-03-11 22:08:52 -0500

    changes to registration

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -11,11 +11,13 @@ import { UserDataService } from "../user-data.service";
 export class RegisterComponent implements OnInit {
 
   user: User = new User();
+  userName: string = "";
   password : string = "";
   password2 : string = "";
   email : string = "";
   age : Number = 0;
   errorMSG: string = '';
+  termsAndConditions: boolean = false;
 
 
   constructor(private data: UserDataService, private router: Router) { }
@@ -24,10 +26,19 @@ export class RegisterComponent implements OnInit {
   }
 
   onSubmit(): void {
-    this.user.Email = this.email;
-    this.user.Password = this.password;
-    this.user.IsAdmin = false;
-    this.data.register(this.user).subscribe(() => this.router.navigate(['/home']))
+    if(this.termsAndConditions == true){
+      this.user.UserName = this.userName;
+      this.user.Email = this.email;
+      this.user.Password = this.password;
+      this.user.IsAdmin = false;
+      this.user.Date = new Date();
+      this.user.ProfilePic = "default.png";
+      this.user.Balance = 5000;
+      this.data.register(this.user).subscribe(() => this.router.navigate(['']))
+    }else{
+      this.errorMSG = "Accept the Terms and Conditions to create account."
+    }
+    
   }
 
 }
--- src/app/user-data.service.ts
+++ src/app/user-data.service.ts
@@ -11,44 +11,29 @@ export class UserDataService {
   constructor(private http: HttpClient) { }
 
   getUsers():Observable<User[]> {
-<<<<<<< HEAD
-    const perPage = '' + Number.MAX_SAFE_INTEGER;
+    const perPage = '' + 50;
     return this.http.get<User[]>(`${API_URI}/v1/users?page=1&perPage=${perPage}`);
   }
 
   getUserById(userId : string):Observable<User>{
     return this.http.get<User>(`${API_URI}/v1/users/${userId}`);
-=======
 
-    let params = {
-      page: "1",
-      perPage: "50"
-    }
-
-    return this.http.get<User[]>(`https://allinsserservice.herokuapp.com/v1/users?`, {params});
->>>>>>> 1e6c341ac1537814f24ad14f42d2afd1d3e1fe9f
   }
 
   register(newUser: User):Observable<any>{
     return this.http.post<any>(`${API_URI}/v1/user`, newUser);
   }
 
-<<<<<<< HEAD
   update(user:User):Observable<any>{
     return this.http.put<any>(`${API_URI}/v1/users/${user._id}`,user);
   }
 
-
-=======
   getAllUsers():Observable<User[]> {
     const perPage = Number.MAX_SAFE_INTEGER.toString();
-
     let params = {
       page: "1",
       perPage: perPage
     }
-
-    return this.http.get<User[]>(`https://allinsserservice.herokuapp.com/v1/users?`, {params});
+    return this.http.get<User[]>(`${API_URI}/v1/users?`, {params});
   }
->>>>>>> 1e6c341ac1537814f24ad14f42d2afd1d3e1fe9f
 }
